a422817735ce4a646be57d203fda6c56e5fbe306
Alex Nicholson <alex.n@clove.kitchen>
2025-01-16 14:28:47 +1100

    Update to include proper dependencies

--- .github/actions/sync-version.js
+++ .github/actions/sync-version.js
@@ -4,17 +4,10 @@ import fs from 'fs'
 let packageJson = fs.readFileSync('package.json');
 let packageObj = JSON.parse(packageJson);
 
-// Specify your dependency
-let dependency = 'lucide-react-native';
+let dependencyVersion = packageObj.dependencies['lucide-react-native'];
 
-// Get the version of the dependency
-let dependencyVersion = packageObj.devDependencies[dependency];
-
-// Update the version of your package
 packageObj.version = dependencyVersion;
 
-// also set the lucide version to the same version
 packageObj.devDependencies['lucide'] = dependencyVersion;
 
-// Write the updated package.json file back to disk
 fs.writeFileSync('package.json', JSON.stringify(packageObj, null, 2));
