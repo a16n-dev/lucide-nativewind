1fc06bf023780ff72fadccdb935a7967e7f84a0c
Alex Nicholson <alex.n@clove.kitchen>
2025-01-16 14:23:06 +1100

    Fix generation script

--- .github/actions/sync-version.js
+++ .github/actions/sync-version.js
@@ -13,5 +13,8 @@ let dependencyVersion = packageObj.devDependencies[dependency];
 // Update the version of your package
 packageObj.version = dependencyVersion;
 
+// also set the lucide version to the same version
+packageObj.devDependencies['lucide'] = dependencyVersion;
+
 // Write the updated package.json file back to disk
 fs.writeFileSync('package.json', JSON.stringify(packageObj, null, 2));
